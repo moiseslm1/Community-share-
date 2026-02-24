17c5750743efc26e47a3a28556ceffbfb7bcec43
efernandez73 <edgarfernandez2002@gmail.com>
2026-02-24 10:40:22 -0800

    Update views.py

--- core/views.py
+++ core/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+  return HttpResponse("Welcome to Community Share! Choose: Request Help or Offer Service")
+
+def feed(request):
+  return HttpResponse("This is the feed page.")
 
 # Create your views here.
