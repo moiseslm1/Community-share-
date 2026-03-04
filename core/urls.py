995fe8da84c8605904a33ffa9157416c914dc4ef
Edgar Fernandez <edgarfernandez2002@gmail.com>
2026-03-04 12:54:00 -0800

    update files before pulling

--- core/urls.py
+++ core/urls.py
@@ -4,8 +4,4 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("feed/", views.feed, name="feed"),
-<<<<<<< HEAD
 ]
-=======
-]
->>>>>>> bd3f32f61ee5cc00bb1938241b355da2a1405168
