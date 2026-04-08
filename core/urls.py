4d13f637acbb5f8aa6a164ba03faa655b2b20d87
Akaris224 <Halloweengreen2003@yahoo.com>
2026-04-08 00:38:13 -0700

    Add navigation improvements and new category/result pages

--- core/urls.py
+++ core/urls.py
@@ -4,9 +4,11 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("feed/", views.feed, name="feed"),
+    path("results/", views.results, name="results"),
     path("guidelines/", views.guidelines, name="guidelines"),
     path("offer/", views.offer, name="offer"),
     path("request/", views.request, name="request"),
     path("events/", views.events, name="events"),
+    path("category/<str:category_slug>/", views.category, name="category"),
     path('jobs/create/', views.offer, name='create_job'),
 ]
