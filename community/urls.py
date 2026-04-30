610e7c0f583f4d1f664812074b33f65d21abe3a2
nperezhernandez <xperez.nallely@gmail.com>
2026-04-29 18:41:59 -0700

    Media URLs in debug mode

--- community/urls.py
+++ community/urls.py
@@ -22,3 +22,6 @@ urlpatterns = [
     path("", include("core.urls")),
     path('', include('django.contrib.auth.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
