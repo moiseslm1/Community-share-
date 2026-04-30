16c3c55e4b3934f86fe7856693fbe46c05673c69
nperezhernandez <xperez.nallely@gmail.com>
2026-04-29 19:20:29 -0700

    Delete Community-share--main directory
    Instead of adding to the repo, accidently readded entire project

--- Community-share--main/core/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import JobListing, Service, Booking, Review
-
-admin.site.register(JobListing)
-admin.site.register(Service)
-admin.site.register(Booking)
-admin.site.register(Review)
